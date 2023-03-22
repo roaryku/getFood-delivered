76ee28057af0f3a40fadfd9bac002b5a0d0a2397
Rodika <raryku@yahoo.com>
2023-03-21 17:06:47 -0700

    key added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -9,7 +9,7 @@ const Cart = () => {
         <div>
             <img className='cartIcon' src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/null/external-shopping-cart-ecommerce-kiranshastry-solid-kiranshastry.png" alt="cartIcon"/>
             <h3>TOTAL: $ {totalPrice}</h3>
-            {cartItems.map(cartItem => <CartItem cartItem={cartItem}/>)}
+            {cartItems.map((cartItem, id) => <CartItem cartItem={cartItem} key={id}/>)}
         </div>
     )
 }
